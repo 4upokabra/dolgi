562c8ab521119c2479b75fe7966f13d26ea4b048
Michail Novikov <novikov_mihail@cybersec74.ru>
2024-12-24 16:41:08 +0500

    change some files

--- raylib_clock_17.09/raylib_clock_17.09/Clock.cpp
+++ raylib_clock_17.09/raylib_clock_17.09/Clock.cpp
@@ -10,9 +10,9 @@ void DrawClock(int screenWidth, int screenHeight) {
     time_t now = time(0);
     tm* ltm = localtime(&now);
 
-    float hourAngle = (ltm->tm_hour % 12 + ltm->tm_min / 60.0) * 30.0 * DEG2RAD;
-    float minuteAngle = (ltm->tm_min + ltm->tm_sec / 60.0) * 6.0 * DEG2RAD;
-    float secondAngle = ltm->tm_sec * 6.0 * DEG2RAD;
+    float hourAngle = (ltm->tm_hour % 12 + ltm->tm_min / 60.0) * 30.0 * DEG2RAD - PI / 2;
+    float minuteAngle = (ltm->tm_min + ltm->tm_sec / 60.0) * 6.0 * DEG2RAD - PI / 2;
+    float secondAngle = ltm->tm_sec * 6.0 * DEG2RAD - PI / 2;
 
     int centerX = screenWidth / 2;
     int centerY = screenHeight / 2;
@@ -26,7 +26,7 @@ void DrawClock(int screenWidth, int screenHeight) {
 
     DrawCircle(centerX, centerY, 200, LIGHTGRAY);
     for (int i = 0; i < 12; ++i) {
-        float angle = i * 30.0 * DEG2RAD;
+        float angle = i * 30.0 * DEG2RAD - PI / 2;
         int x = centerX + 180 * cos(angle);
         int y = centerY + 180 * sin(angle);
         DrawText(TextFormat("%d", i == 0 ? 12 : i), x - 10, y - 10, 20, DARKGRAY);
